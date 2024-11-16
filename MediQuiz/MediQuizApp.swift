0ae0f7fe6a97b0b26fe509dd7bcbeabb0d036b22
Igor Pajkert <pajkert.igor@gmail.com>
2024-11-16 18:40:52 +0100

    Initial Commit

---
+++ MediQuiz/MediQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediQuizApp.swift
+//  MediQuiz
+//
+//  Created by Igor Pajkert on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MediQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
